b8fd8fce431c1eedfe2a0bd687638ba53a677e16
Artem2406 <99211331+Artem2406@users.noreply.github.com>
2022-05-04 13:51:54 +0300

    Add files via upload

---
+++ Lab4/JImageDisplay.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class JImageDisplay extends JComponent {
+
+    private BufferedImage displayImage;
+
+    public JImageDisplay(int height, int width){
+        displayImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Dimension imageDimension = new Dimension(width, height);
+        super.setPreferredSize(imageDimension);
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage (displayImage, 0, 0, displayImage.getWidth(), displayImage.getHeight(), null);
+    }
+
+    public void clearImage(){
+        int[] blankArray = new int[getWidth() * getHeight()];
+        displayImage.setRGB(0, 0, getWidth(), getHeight(), blankArray, 0, 1);
+    }
+
+    public void drawPixel(int x, int y, int rgbColor)
+    {
+        displayImage.setRGB(x, y, rgbColor);
+    }
+}
